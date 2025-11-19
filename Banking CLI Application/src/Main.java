deb73baf85b2500b7eab749922685e0dc653d181
Shafeeq S <shafeeq07official@gmail.com>
2025-11-19 10:16:59 +0530

    Refactor application start method in Main class

--- Banking CLI Application/src/Main.java
+++ Banking CLI Application/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // start the application
-        BankingApplication bankingApplication = new BankingApplication();
-        bankingApplication.startApplication();
+        // start the application in the main class
+        BankingApplication.startApplication();
     }
 }
